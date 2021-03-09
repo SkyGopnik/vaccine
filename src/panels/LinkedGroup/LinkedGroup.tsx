9c4efc6ce76738755236abff64cfe8e9076e022c
R-o-t-I <49370598+R-o-t-I@users.noreply.github.com>
2021-03-09 23:07:14 +0300

    ref

---
+++ src/panels/LinkedGroup/LinkedGroup.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Panel,
+  PanelHeader,
+  Div,
+  Text,
+  SimpleCell,
+  Avatar
+} from "@vkontakte/vkui";
+
+import HistoryBackBtn from "src/components/HistoryBackBtn";
+import Card from 'src/components/Card/Card';
+
+import Img5 from "src/img/profile/5.svg";
+
+import style from "./LinkedGroup.scss";
+
+interface IProps {
+  id: string
+}
+
+export default class extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {};
+  }
+
+
+  render() {
+    const { id } = this.props;
+
+    return (
+      <Panel id={id}>
+        <PanelHeader left={<HistoryBackBtn />} separator={false}>
+          Сообщество
+        </PanelHeader>
+        <Div className={style.block}>
+          <Text weight="regular" style={{ marginBottom: 16 }}>Ты можешь создавать вакцину вместе с подписчиками выбранного сообщества.</Text>
+          <Card
+            icon={<img src={Img5} alt="" />}
+            title="Моё сообщество"
+          >
+            <SimpleCell 
+              before={
+                <Avatar size={40}  />
+              }
+              expandable
+              after="Изм."
+              description="182 учёных"
+            >
+              SkyRegils Studio
+            </SimpleCell>
+          </Card>
+        </Div>
+      </Panel>
+    );
+  }
+}
---
+++ src/panels/Ref/Ref.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  Panel,
+  PanelHeader,
+  Div,
+  Title,
+  Text,
+  Subhead,
+  Header,
+  Input,
+  Button
+} from "@vkontakte/vkui";
+
+import HistoryBackBtn from "src/components/HistoryBackBtn";
+import Card from 'src/components/Card/Card';
+
+import Img5 from "src/img/profile/5.svg";
+import Img6 from "src/img/profile/6.svg";
+
+import { Icon28Send } from '@vkontakte/icons';
+
+import style from "./Ref.scss";
+
+interface IProps {
+  id: string
+}
+
+export default class extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {};
+  }
+
+
+  render() {
+    const { id } = this.props;
+
+    return (
+      <Panel id={id}>
+        <PanelHeader left={<HistoryBackBtn />} separator={false}>
+          Спасиние друзей
+        </PanelHeader>
+        <Div className={style.block}>
+          <Text weight="regular" style={{ marginBottom: 16 }}>Скопируй код и покажи его другу. За каждое приглашение ты и твой друг получите бонусную вакцину. Спаси своих друзей!</Text>
+          <Card>
+            <div className={style.ref}>
+              <Title level="1" weight="semibold">11928</Title>
+              <Subhead weight="regular">Нажми на код, чтобы скопировать его</Subhead>
+            </div>
+          </Card>
+          <Card
+            icon={<img src={Img5} alt="" />}
+            title="Статистика"
+          >
+            <Subhead weight="regular">
+              <div>· Спасено друзей: </div>
+              <div>· Получено вакцины от друзей: </div>
+            </Subhead>
+          </Card>
+        </Div>
+        <Div className={style.block}>
+          <Header mode="secondary">У меня есть код приглашения</Header>
+          <Text weight="regular" style={{ marginBottom: 16 }}>Есть код? Ура — ты спасён. Осталось лишь ввести этот код от друга сюда. За это ты получишь подарок.</Text>
+          <Card
+            icon={<img src={Img6} alt="" />}
+            title="Ввести код"
+          >
+            <Subhead weight="regular">
+              <div className={style.input}>
+                <Input placeholder="11928" />
+                <Button className={style.input}>
+                  <Icon28Send />
+                </Button>
+              </div>
+            </Subhead>
+          </Card>
+        </Div>
+      </Panel>
+    );
+  }
+}
