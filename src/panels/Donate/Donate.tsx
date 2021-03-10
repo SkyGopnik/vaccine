0f152d46dbacc7f5035cd10dab651a7eabb1d2c2
Artyom <artem.petrunin@list.ru>
2021-03-11 01:25:55 +0300

    add tabs

--- src/panels/Donate/Donate.tsx
+++ src/panels/Donate/Donate.tsx
@@ -22,63 +22,70 @@ interface IProps {
   id: string
 }
 
-export default class extends React.Component<IProps> {
+interface IState {
+  type: 'sub' | 'vaccine'
+}
+
+export default class extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
 
-    this.state = {};
-  }
-
-  select(e) {
-    const mode = e.currentTarget.dataset.mode;
-    this.setState({ mode, contextOpened: false });
+    this.state = {
+      type: 'sub'
+    };
   }
 
   render() {
     const { id } = this.props;
+    const { type } = this.state;
 
     return (
       <Panel id={id}>
         <PanelHeader left={<HistoryBackBtn />} separator={false}>
           Магазин
         </PanelHeader>
-        <FixedLayout className={style.subHeader} vertical="top">
+        <FixedLayout vertical="top">
           <Tabs>
             <TabsItem
-              onClick={() => this.changeType('sub')}
+              onClick={() => this.setState({ type: 'sub' })}
               selected={type === 'sub'}
             >
               Подписка
             </TabsItem>
             <TabsItem
-              onClick={() => this.changeType('buy')}
-              selected={type === 'buy'}
+              onClick={() => this.setState({ type: 'vaccine' })}
+              selected={type === 'vaccine'}
             >
               Купить вакцину
             </TabsItem>
           </Tabs>
         </FixedLayout>
-        <Div className={style.block}>
-          <Text weight="regular" style={{ marginBottom: 16 }}>Получите сверхпреимущества игры, оформив подписку. Играть станет интереснее, а разработчики смогут немного покушать ;)</Text>
-          <Card>
-            <Headline weight="medium" className={style.headline}>Premium за 69₽/мес</Headline>
-            <Text weight="regular" className={style.dark}>
-              <div>· 10 млн вакцины, 2 маски и 2 респиратора раз в неделю</div>
-              <div>· место в рейтинге донов в игре</div>
-              <div>· набор стикеров ВКонтакте раз в месяц</div>
-            </Text>
-          </Card>
-          <Card>
-            <Headline weight="medium" className={style.headline}>Premium за 99₽/мес</Headline>
-            <Text weight="regular" className={style.dark}>
-              <div>· 10 млн вакцины, 2 маски и 2 респиратора раз в неделю</div>
-              <div>· место в рейтинге донов в игре</div>
-              <div>· набор стикеров ВКонтакте раз в месяц</div>
-              <div>· чай, кофе, шаверма с доставкой</div>
-            </Text>
-          </Card>
-          <Caption level="1" weight="regular">Подписка оформляется через VK Donut — сервис ВКонтакте. Её можно отключить в сообществе игры в любой момент. <Link>Читать подробнее</Link></Caption>
-        </Div>
+        {type === 'sub' && (
+          <Div className={style.block}>
+            <Text weight="regular" style={{ marginBottom: 16 }}>Получите сверхпреимущества игры, оформив подписку. Играть станет интереснее, а разработчики смогут немного покушать ;)</Text>
+            <Card>
+              <Headline weight="medium" className={style.headline}>Premium за 69₽/мес</Headline>
+              <Text weight="regular" className={style.dark}>
+                <div>· 10 млн вакцины, 2 маски и 2 респиратора раз в неделю</div>
+                <div>· место в рейтинге донов в игре</div>
+                <div>· набор стикеров ВКонтакте раз в месяц</div>
+              </Text>
+            </Card>
+            <Card>
+              <Headline weight="medium" className={style.headline}>Premium за 99₽/мес</Headline>
+              <Text weight="regular" className={style.dark}>
+                <div>· 10 млн вакцины, 2 маски и 2 респиратора раз в неделю</div>
+                <div>· место в рейтинге донов в игре</div>
+                <div>· набор стикеров ВКонтакте раз в месяц</div>
+                <div>· чай, кофе, шаверма с доставкой</div>
+              </Text>
+            </Card>
+            <Caption level="1" weight="regular">Подписка оформляется через VK Donut — сервис ВКонтакте. Её можно отключить в сообществе игры в любой момент. <Link>Читать подробнее</Link></Caption>
+          </Div>
+        )}
+        {type === 'vaccine' && (
+          <div>123</div>
+        )}
       </Panel>
     );
   }
